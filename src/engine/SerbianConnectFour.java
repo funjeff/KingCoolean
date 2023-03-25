b3be25534defa2b02a8cc0a6c66d92053b09a555
Sam Husted <samuelhusted7@gmail.com>
2023-03-25 14:12:32 -0500

    FINALLY Implemented the FUCKING ALGORITHM! NO MORE LEFT LARRY!

--- src/engine/SerbianConnectFour.java
+++ src/engine/SerbianConnectFour.java
@@ -16,7 +16,7 @@ public class SerbianConnectFour {
 					input = 
 							(player == 1) ? 
 							s.nextInt() 
-							: scf.alphaBetaSearch(scf.board, 10)
+							: scf.alphaBetaSearch(scf.board, 4)
 							;
 //					System.out.println(input >= 0);
 //					System.out.println(input <= 6);
@@ -65,6 +65,25 @@ public class SerbianConnectFour {
 		}
 		return false;
 	}
+	public boolean checkForNextTurnWin(int[][] board, int player) {
+		int[][] temp = new int[6][7];
+		for (int x = 0; x < 6; x++) {
+			for (int i = 0; i < 6; i++) {
+				for (int j = 0; j < 7; j++) {
+					temp[i][j] = board[i][j];
+				}
+			}
+			if (!isFullColumn(board, x)) {
+				add(temp, x, player);
+				if (checkForWin(temp) == player) return true;
+			}
+		}
+		return false;
+	}
+	
+	private boolean isFullColumn(int[][] board, int colX) {
+	    return board[0][colX] != 0;
+	  }
 	
 	public int checkForWin(int[][] board) {
 		int check, count = 1;
@@ -106,31 +125,143 @@ public class SerbianConnectFour {
 	}
 	
 	public int getHeuristic(int[][] board) {
-		int count = 1, heuristic = 88;
-		for (int wx = 0; wx < 6; wx++) {
-			for (int wy = 0; wy < 7; wy++) {
-				if (board[wx][wy] != 0) {
-					for (int q = 0; q < 4; q++) {
-						int xMove = wx + xDir[q];
-						int yMove = wy + yDir[q];
-						while (xMove > -1 && xMove < 6 && yMove > -1 && yMove < 7) {
-							count++;
-							xMove += xDir[q];
-							yMove += yDir[q];
-						}
-						if (count >= 4) {
-							heuristic -= count - 3;
-						}
-						count = 1;
-					}
-				}
-			}
-		}
-		return heuristic;
+	    int score = 0;
+	    if(checkForNextTurnWin(board, 1) == true) {
+	    	score = 1000;
+	        if(checkForWin(board) == 1) {
+	        	score = 1000000;
+	        }
+
+	    }
+	    else if(checkForNextTurnWin(board, 2) == true) {
+	    	score = -1000;
+	    }
+	    if(checkForWin(board) == 2) {
+	    	score = -1000000;
+	    }
+
+	    int yellowsFour = findNumWaysToWin(board, 2);
+	    int redsFour = findNumWaysToWin(board, 1);
+	    int yellowsThree = findNumThrees(board, 2);
+	    int redsThree = findNumThrees(board, 1);
+
+	    score += 40*(redsFour - yellowsFour) + 10*(redsThree - yellowsThree);
+	    if (score > 0) {
+	    	return score;
+	    }
+	    return score;
 	}
 	
+	public int findNumWaysToWin(int[][] board, int player) {
+		int numWays = 0;
+	    for (int x = 0; x < 6; x++) {
+	      for (int y = 0; y < 7; y++) {
+	        if (board[x][y] == 0) {
+	          numWays += numMakesFour(board, y, x, player);
+	        }
+	      }
+	    }
+	    return numWays;
+	  }
+	
+	  public int numMakesFour(int[][] board, int y, int x, int player) {
+		    int numWays = 0;
+		    for (int i = 0; i < 4; i++) {
+		      if (makesFour(board, y, x, player, i)) {
+		        numWays++;
+		      }
+		    }
+		    return numWays;
+		  }
+
+	  private boolean makesFour(int[][] board, int y, int x, int player, int dir) {
+	    int count = 0;
+	    int x1 = x;
+	    int y1 = y;
+	    while ((x1 > -1 && x1 < 6 && y1 > -1 && y1 < 7) && board[x1][y1] == player) {
+	      x1 += xDir[dir];
+	      y1 += yDir[dir];
+	      count++;
+	    }
+	    x1 = x;
+	    y1 = y;
+	    while ((x1 > -1 && x1 < 6 && y1 > -1 && y1 < 7) && board[x1][y1] == player) {
+	      x1 -= xDir[dir];
+	      y1 -= xDir[dir];
+	      count++;
+	    }
+	    return count - 1 >= 4;
+	  }
+	  public int findNumThrees(int[][] board, int player) {
+		    int numWays = 0;
+		    for (int x = 0; x < 6; x++) {
+		      for (int y = 0; y < 7; y++) {
+		        if (board[x][y] == player) {
+		          numWays += numMakesThree(board, y, x, player);
+		        }
+		      }
+		    }
+		    return numWays;
+		  }
+
+		  private int numMakesThree(int[][] board, int y, int x, int player) {
+		    int numWays = 0;
+		    for (int i = 0; i < 4; i++) {
+		      if (makesThree(board, y, x, player, i)) {
+		        numWays++;
+		      }
+		    }
+		    return numWays;
+		  }
+
+		  private boolean makesThree(int[][] board, int y, int x, int player, int dir) {
+		    int count = 0;
+		    int x1 = x;
+		    int y1 = y;
+		    while ((x1 > -1 && x1 < 6 && y1 > -1 && y1 < 7) && board[x1][y1] == player) {
+		      x1 += xDir[dir];
+		      y1 += yDir[dir];
+		      count++;
+		    }
+		    x1 = x;
+		    y1 = y;
+		    while ((x1 > -1 && x1 < 6 && y1 > -1 && y1 < 7) && board[x1][y1] == player) {
+		      x1 -= xDir[dir];
+		      y1 -= yDir[dir];
+		      count++;
+		    }
+		    return count - 1 >= 3;
+		  }
+//	public int getHeuristic(int[][] board) {
+//		int check, count = 1, heuristic = 88;
+//		for (int wx = 0; wx < 6; wx++) {
+//			for (int wy = 0; wy < 7; wy++) {
+//				if (board[wx][wy] != 0) {
+//					check = board[wx][wy];
+//					for (int q = 0; q < 4; q++) {
+//						int xMove = wx + xDir[q];
+//						int yMove = wy + yDir[q];
+//						while (xMove > -1 && xMove < 6 && yMove > -1 && yMove < 7) {
+//							if (board[xMove][yMove] == 0 || 
+//									board[xMove][yMove] == 
+//									((check == 1) ? 2 : 1)) break;
+//							count++;
+//							xMove += xDir[q];
+//							yMove += yDir[q];
+//						}
+//						if (count >= 4) {
+//							heuristic += (check == 1) ? -(Math.pow());
+//						}
+//						count = 1;
+//					}
+//				}
+//			}
+//		}
+//		return heuristic;
+//	}
+	
 	public int alphaBetaSearch(int[][] board, int difficulty) {
-		int a = Integer.MAX_VALUE, b = Integer.MIN_VALUE;
+		int a = Integer.MIN_VALUE, b = Integer.MAX_VALUE;
 		State s = new State(-1, board, 2);
 		int val = maxValue(s, a, b, 0, difficulty);
 		return s.findChildPlay(val);
@@ -149,8 +280,12 @@ public class SerbianConnectFour {
 						childBoard[wx][wy] = parent.board[wx][wy];
 					}
 				}
-				add(childBoard, i, ((parent.getPlayer() == 1) ? 2 : 1));
-				State s = new State(i, childBoard, ((parent.getPlayer() == 1) ? 2 : 1));
+				add(childBoard, i, parent.getPlayer());
+				State s;
+				if (parent.getPlayer() == 1) {
+					s = new State(i, childBoard, 2);
+				}
+				else s = new State(i, childBoard, 1);
 				parent.addChild(s);
 				v = Math.max(v, minValue(s, a, b, depth + 1, difficulty));
 				if (v >= b) {
@@ -170,7 +305,7 @@ public class SerbianConnectFour {
 			return parent.getValue();
 		}
 		else {
-			int v = Integer.MIN_VALUE;
+			int v = Integer.MAX_VALUE;
 			for (int i = 0; i < 6; i++) {
 				int[][] childBoard = new int[6][7];
 				for (int wx = 0; wx < 6; wx++) {
@@ -178,8 +313,12 @@ public class SerbianConnectFour {
 						childBoard[wx][wy] = parent.board[wx][wy];
 					}
 				}
-				add(childBoard, i, ((parent.getPlayer() == 1) ? 2 : 1));
-				State s = new State(i, childBoard, ((parent.getPlayer() == 1) ? 2 : 1));
+				add(childBoard, i, parent.getPlayer());
+				State s;
+				if (parent.getPlayer() == 1) {
+					s = new State(i, childBoard, 2);
+				}
+				else s = new State(i, childBoard, 1);
 				parent.addChild(s);
 				v = Math.min(v, maxValue(s, a, b, depth + 1, difficulty));
 				if (v <= a) {
