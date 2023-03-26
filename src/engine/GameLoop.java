b193d7c18ece362c9d694c7260e5192e1b9780ad
ootisg <ootisg@illcomics.com>
2023-03-26 00:23:51 -0500

    LWJGL integration

--- src/engine/GameLoop.java
+++ src/engine/GameLoop.java
@@ -1,83 +1,82 @@
 package engine;
 
-import java.awt.Rectangle;
-import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.Random;
 
 /**
- * A loop for the game logic; mostly copy-pasted from RenderLoop
+ * The loop for rendering the game to the GameWindow. Runs on the event dispatching thread.
  * @author nathan
  *
  */
-public class GameLoop implements Runnable {
-
+public class RenderLoop {
+	
 	/**
 	 * The maximum framerate the game can run at
 	 */
-	public static final double stepsPerSecond = 30;
+	public static final double maxFramerate = GameLoop.stepsPerSecond;
 	/**
 	 * The time of the last update to the GameWindow, in nanoseconds.
 	 */
 	static private long lastUpdate;
-	/**
-	 * The image of the input from the past GameLogic frame
-	 */
-	static private InputManager inputImage;
-	/**
-	 * Whether or not the game logic has run at least once
-	 */
-	static private volatile boolean gameCodeHasRun = false;
 	
-	@Override
-	public void run () {
-		GameCode.init ();
+	/**
+	 * The system time when this frame's rendering began
+	 */
+	static private long frameTime;
+	
+	public static final GameWindow wind = new GameWindow (960, 540);
+	
+	public static void main (String[] args) {
+		//Sets the initial frame time
+		frameTime = System.currentTimeMillis ();
+		//Start the game logic loop on a separate thread
+		GameLoop gameLoop = new GameLoop ();
+		new Thread (gameLoop).start ();
+		while (!GameLoop.hasRun ()) {/*wait*/}
+		//Initializes lastUpdate to the current time
+		lastUpdate = System.nanoTime ();
 		while (true) {
-			//Get the target time in nanoseconds for this iteration; should be constant if stepsPerSecond doesn't change
-			long targetNanoseconds = (long)(1000000000 / stepsPerSecond);
-			//Get the time before running the game logic
+			//Get the target time in nanoseconds for this iteration; should be constant if the framerate doesn't change
+			long targetNanoseconds = (long)(1000000000 / maxFramerate);
+			//Get the time before refreshing the window
 			long startTime = System.nanoTime ();
-			//doGameLogic
-			inputImage = RenderLoop.wind.getInputImage ();
-			GameCode.beforeGameLogic ();
-			GameCode.gameLoopFunc();
-			RenderLoop.wind.resetInputBuffers ();
-			GameCode.afterGameLogic ();
-			gameCodeHasRun = true;
+			frameTime = System.currentTimeMillis ();
+			//Render the window
+			GameCode.beforeRender ();
+			GameCode.renderFunc();
+			GameCode.afterRender ();
+			wind.refresh ();
 			//Calculate elapsed time and time to sleep for
 			lastUpdate = System.nanoTime ();
 			long elapsedTime = lastUpdate - startTime;
 			int sleepTime = (int)((targetNanoseconds - elapsedTime) / 1000000) - 1;
+			//System.out.println("target " + targetNanoseconds);
+			//System.out.println("elapsed " + elapsedTime);
+			//System.out.println("last " + lastUpdate);
+			//System.out.println("sleep " + sleepTime);
+			//System.out.println("start " + startTime);
+			
 			if (sleepTime < 0) {
 				sleepTime = 0;
 			}
-			//Sleep until ~1ms before it's time to calculate the next step
+			//Sleep until ~1ms before it's time to redraw the frame (to account for inaccuracies in Thread.sleep)
 			try {
 				Thread.currentThread ().sleep (sleepTime);
 			} catch (InterruptedException e) {
 				//Do nothing; the while loop immediately after handles this case well
 			}
-			//Wait until the next step should be executed
+			//Wait until the frame should be redrawn
 			while (System.nanoTime () - startTime < targetNanoseconds) {
-				
+			
 			}
 		}
 	}
 	
 	/**
-	 * Gets the input image from the start of this game logic iteration.
-	 * @return The input image from the start of this iteration
+	 * Gets the value returned by System.currentTimeMillis() at the start of the frame being rendered.
+	 * @return The start time of the current frame
 	 */
-	public static InputManager getInputImage () {
-		return inputImage;
-	}
-	
-	/**
-	 * Returns true if the game logic has been run at least once; false otherwise
-	 */
-	public static boolean hasRun () {
-		return gameCodeHasRun;
+	public static long frameStartTime () {
+		return frameTime;
 	}
 }
